aeb8dd0c0e898a72e5b8758f1678e01dacb020f7
rogergriff <rogergriff121@gmail.com>
2016-03-10 18:47:10 -0800

    Update Part4_Completed.cpp

--- Part4_Completed.cpp
+++ Part4_Completed.cpp
@@ -11,51 +11,6 @@ void drawTri(int n);
 int gcd(int a, int b);
 
 
-int main(){
-	//Fib Variables
-	int response = 0;
-	
-	cout << "Which number of the Fibonnaci Sequence do you want? \n(For this program, we are NOT including 0 as a part of the sequence.)\n";
-	cin >> response;
-	cout << "That number is " << fib(response) << endl; //original call
-
-	//Power Variables
-	int number = 0;
-	int power = 0;
-
-	cout << "Moving on to power. What number do you want raised to what power\n(use format [number space power])" << endl;
-	cin >>  number >> power;
-	cout << "That comes out to " << pow(number, power) << endl; //original call
-	
-	//Tri Variables
-	int triNum = 0;
-	
-	cout << "Moving on to tri. Which triangular number do you want?\n";
-	cin >> triNum;
-	cout << "That comes out to " << tri(triNum) << endl << "Your triangle looks something like this:\n"; //original call
-	drawTri(triNum);  // random function I wanted to do, because why not?
-	
-	//Gcd Variables
-	int gcdNum1;
-	int gcdNum2;
-
-	cout << "Finally, moving on to gcd. Please enter the two numbers you want \n to find the greatest common denominator of. \n(Use formate [number1 space number2]) \n";
-	cin >> gcdNum1 >> gcdNum2;
-	if (gcdNum1 < 0 || gcdNum2 < 0){
-		gcdNum1 = abs(gcdNum1);
-		gcdNum2 = abs(gcdNum2);
-	}
-	cout << "The greatest common denominator between these two numbers is:\n" << gcd(gcdNum1, gcdNum2) << endl; // original call
-
-	char repeat;
-	cout << "Would you like to run this program again? \nRespond Y for yes, or any other character for no." << endl;
-	cin >> repeat;
-	repeat = tolower(repeat);
-	if (repeat == 'y')
-		main(); //recursively calling main, because I wanna :P.
-	return 0;
-}
-
 int fib(int n){
 	if (n == 1)
 		return 1;
