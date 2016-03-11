9229707f2353ddb7327a0e59c6d4ba11089dcab4
rogergriff <rogergriff121@gmail.com>
2016-03-10 18:49:54 -0800

    Update Completed_Part5.cpp
    Making it so all we have to do is upload this file

--- Completed_Part5.cpp
+++ Completed_Part5.cpp
@@ -1,48 +1,4 @@
-#include<iostream>
-#include<cmath>
-using std::cin;
-using std::cout;
-using std::endl;
-int fib_iter(int n);
-int pow_iter(int base, int exp);
-int tri_itter(int n);
-int gcd_itter(int num1, int num2);
-int main() {
-	//Fib Variables
-	int number;
-	cout << "Which number of the Fibonnaci Sequence do you want? \n(For this program, we are NOT including 0 as a part of the sequence.)\n";
-	cin >> number;
-	cout << "That number is " << fib_iter(number) << endl;
-	 
-	//Pow Variables
-	int base;
-	int exp;
-	cout << "Moving on to power. What number do you want raised to what power\n(use format [number space power])" << endl;
-	cin >> base >> exp;
-	cout << "That comes out to " << pow_iter(base, exp) << endl;
-
-	//Tri Variables
-	int number2;
-	cout << "Moving on to tri. Which triangular number do you want?\n";
-	cin >> number2;
-	cout << "That comes out to " << tri_itter(number2) << endl;
-
-	//GCD Variables
-
-	int gcdNum1;
-	int gcdNum2;
-
-	cout << "Finally, moving on to gcd. Please enter the two numbers you want \n to find the greatest common denominator of. \n(Use formate [number1 space number2]) \n";
-	cin >> gcdNum1 >> gcdNum2;
-	cout << "The greatest common denominator between these two numbers is:\n" << gcd_itter(gcdNum1, gcdNum2) << endl;
-
-
-	return 0;
-}
-
-
 int fib_iter(int n) {
-
 	int a = 0;
 	int b = 1;
 	int sum;
